00701cbc68ec68c8bd4cedef70fdf80c228ccdae
RubikIdeaCom <rubikideacom@gmail.com>
2023-05-19 23:44:54 +0330

    Facade design pattern

---
+++ Facade/GooglePlus.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'ShareInterface.php';
+class GooglePlus implements ShareInterface
+{
+    private $message;
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function share()
+    {
+        echo "Sharing by $this->message on Google+.<br/>";
+    }
+}
---
+++ Facade/ShareInterface.php
@@ -0,0 +1,6 @@
+<?php
+interface ShareInterface
+{
+    public function setMessage($message);
+    public function share();
+}
